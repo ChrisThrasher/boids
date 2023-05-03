a3eb6a18b4d414bd069b9bf58132964cae59675c
Chris Thrasher <chrisjthrasher@gmail.com>
2023-05-03 13:39:50 -0600

    Use correct result type

--- src/Boid.cpp
+++ src/Boid.cpp
@@ -6,7 +6,7 @@
 #include <random>
 
 std::mt19937 rng = [] {
-    auto seed_data = std::array<int, std::mt19937::state_size>();
+    auto seed_data = std::array<std::random_device::result_type, std::mt19937::state_size>();
     auto rd = std::random_device();
     std::generate_n(seed_data.data(), seed_data.size(), std::ref(rd));
     auto seed_seq = std::seed_seq(seed_data.begin(), seed_data.end());
