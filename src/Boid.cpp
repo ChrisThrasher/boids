961248302fbd01aeb1e003b4a0ec6781d7c2a538
Chris Thrasher <chrisjthrasher@gmail.com>
2024-05-01 12:22:53 -0600

    Put anonymous namespace at top of file

--- src/Boid.cpp
+++ src/Boid.cpp
@@ -6,18 +6,6 @@
 #include <numeric>
 #include <random>
 
-std::mt19937& rng()
-{
-    thread_local auto rng = []() {
-        auto seed_data = std::array<std::random_device::result_type, std::mt19937::state_size>();
-        auto rd = std::random_device();
-        std::generate_n(seed_data.data(), seed_data.size(), std::ref(rd));
-        auto seed_seq = std::seed_seq(seed_data.begin(), seed_data.end());
-        return std::mt19937(seed_seq);
-    }();
-    return rng;
-}
-
 namespace {
 auto brightness_dist = std::uniform_int_distribution<uint16_t>(128, 255);
 auto speed_dist = std::uniform_real_distribution(250.f, 500.f);
@@ -29,6 +17,18 @@ auto clamp(const sf::Vector2f& vector, const float min, const float max)
         return vector;
     return vector.normalized() * std::clamp(length, min, max);
 }
+}
+
+std::mt19937& rng()
+{
+    thread_local auto rng = []() {
+        auto seed_data = std::array<std::random_device::result_type, std::mt19937::state_size>();
+        auto rd = std::random_device();
+        std::generate_n(seed_data.data(), seed_data.size(), std::ref(rd));
+        auto seed_seq = std::seed_seq(seed_data.begin(), seed_data.end());
+        return std::mt19937(seed_seq);
+    }();
+    return rng;
 }
 
 Boid::Boid(const sf::Vector2f& position, const sf::Angle& rotation)
