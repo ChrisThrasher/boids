cdb5ddd5b17761b495761cf71494ecab632cfe54
Chris Thrasher <chrisjthrasher@gmail.com>
2024-12-08 14:19:36 -0700

    Fix clang-tidy error

--- src/Boid.cpp
+++ src/Boid.cpp
@@ -21,7 +21,7 @@ auto clamp(const sf::Vector2f vector, const float min, const float max)
 
 std::mt19937& rng()
 {
-    thread_local auto rng = []() {
+    static thread_local auto rng = []() {
         auto seed_data = std::array<std::random_device::result_type, std::mt19937::state_size>();
         auto rd = std::random_device();
         std::generate_n(seed_data.data(), seed_data.size(), std::ref(rd));
